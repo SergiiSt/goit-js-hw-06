03da96392c7ba01ed828df8bb75094d1d847d892
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-16 15:05:41 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const spanColorEl = document.querySelector('.color');
+const changeColoprBtn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+changeColoprBtn.addEventListener('click', () => {
+  spanColorEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = spanColorEl.textContent;
+})
