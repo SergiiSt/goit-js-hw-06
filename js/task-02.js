7335e4fe68ec167bee1bac94b83e65562f08eb06
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-16 12:03:01 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (const ingridient of ingredients) {
+  const ingItemEl = document.createElement("li");
+  ingItemEl.textContent = ingridient;
+  ingItemEl.classList = "item";
+  console.log(ingItemEl);
+  const list = document.getElementById('ingredients')
+  list.append(ingItemEl)
+}
