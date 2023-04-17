79022d51d4a96afd1beb6e27f9a6bff665d8c088
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-18 00:41:48 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    boxSize += 10;
+  }
+}
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+
+createButton.addEventListener('click', () => {
+  createBoxes(Number(input.value));
+});
+
+destroyButton.addEventListener('click', () => {
+  destroyBoxes();
+});
