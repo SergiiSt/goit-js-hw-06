3393e479b65af42dc221aa817bf701bfd0a5dfb5
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-17 23:49:45 +0200

    fix task5

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
-const inputEl = document.getElementById('name-input');
-const spanEl = document.getElementById('name-output');
-inputEl.addEventListener('input', onInputChange);
+const inputEl = document.getElementById("name-input");
+const spanEl = document.getElementById("name-output");
+inputEl.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-    console.log(event.currentTarget.value)
-    spanEl.textContent = event.currentTarget.value
+  const inputValue = event.target.value.trim();
+  spanEl.textContent = inputValue ? inputValue : "Anonymous";
 }
