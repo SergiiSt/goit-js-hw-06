aec58539d6a400a9592096a0ab584f8309697d1d
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-17 23:03:49 +0200

    fix task1

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,8 @@ const newArray = categoriesList.children;
 console.log(`Number of categories: ${newArray.length}`);
 const subCatList = categoriesList.querySelectorAll(".item");
 subCatList.forEach((item) => {
-  const elementName = item.querySelector("h2").textContent;
-  const elementsCount = item.querySelectorAll("li").length;
+  const elementName = item.firstElementChild.textContent;
+  const elementsCount = item.lastElementChild.children.length;
   console.log(`Category: ${elementName}`);
   console.log(`Elements: ${elementsCount}`);
 });
