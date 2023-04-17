af93134637b663ead909ed765e2920669679b89c
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2023-04-18 00:09:57 +0200

    fix task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const rangeInput = document.getElementById('font-size-control');
 const textToChange = document.getElementById('text');
 rangeInput.addEventListener('input', onRangeChange);
+textToChange.style.fontSize = `${rangeInput.value}px`
 function onRangeChange(event) {
   textToChange.style.fontSize = `${event.currentTarget.value}px`
 }
